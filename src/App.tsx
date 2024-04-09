35d9a3f1ee8491057e680958a71167dc6b2ad836
samzyconcepts <samson.oderinwale@kibo.school>
2024-04-10 00:23:14 +0100

    Update: input reuseable component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     return (
         <>
             <form className="container mx-auto flex gap-4">
-                <Input />
+                <Input type="text" placeholder="Add your task..."/>
                 <Button variant="primary" className="h-14 w-14 flex items-center justify-center">
                 <img src={send} alt="arrow-button" />
                 </Button>
--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -1,4 +1,15 @@
-const Input = () => {
-    return <input className="border" type="text" placeholder="Add your task..." />;
+type InputProps = React.HTMLAttributes<HTMLInputElement> & {
+    type: string;
+    placeholder: string;
+};
+
+const Input = ({ type, placeholder }: InputProps) => {
+    return (
+        <input
+            className="px-4 py-2 rounded-lg border outline-[#855DDE]"
+            type={type}
+            placeholder={placeholder}
+        />
+    );
 };
 export default Input;
