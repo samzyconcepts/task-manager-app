40e0e2caa310c863d32c7c8b3ded6ffdd2bf8dba
samzyconcepts <samson.oderinwale@kibo.school>
2024-04-10 00:39:03 +0100

    Update: add the form component inside the main app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import Button from "./component/ui/Button";
-import Input from "./component/ui/Input";
-
-// Image
-import send from "./assets/send.svg";
+import { Form } from "./component/Form";
 
 function App() {
     return (
         <main className="grid place-content-center h-lvh">
-            <form className="container mx-auto flex gap-4">
-                <Input type="text" placeholder="Add your task..." className="w-[505px]" />
-                <Button variant="primary" className="h-14 w-14 flex items-center justify-center">
-                    <img src={send} alt="arrow-button" />
-                </Button>
-            </form>
+            <Form/>
         </main>
     );
 }
