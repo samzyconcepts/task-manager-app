3bae86bcad9c2f37f1b60f640dbf538354c063c3
samzyconcepts <samson.oderinwale@kibo.school>
2024-04-10 00:05:35 +0100

    Update: creating reuseable button component

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,10 +1,29 @@
-import send from "../assets/send.svg";
+import { cva } from "class-variance-authority";
+import { cn } from "../util/cn";
 
-const Button = () => {
-    return (
-        <button type="submit">
-            <img src={send} alt="arrow-button" />
-        </button>
-    );
+type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
+    variant?: "primary" | "secondary";
 };
-export default Button;
+
+export default function Button({ className, variant, ...props }: ButtonProps) {
+    return <button {...props} className={cn(buttonVariants({ variant }), className)} />;
+}
+
+// cva (class variance authority) is a library that allows you to configure different variance for a button each with is own style. It makes our button reuseable with different style.
+
+// to make the component extensible with another style outside what we have specified here. we will give access to the className and also create util file cn.ts...
+
+const buttonVariants = cva(
+    "py-2 px-4 rounded-lg hover:bg-[#855DDE] transition-colors duration-150",
+    {
+        variants: {
+            variant: {
+                primary: "bg-[#BEAEE2] text-white",
+                secondary: "bg-white text-black border-2 border-black",
+            },
+        },
+        defaultVariants: {
+            variant: "primary",
+        },
+    }
+);
