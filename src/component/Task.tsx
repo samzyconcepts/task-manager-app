5ce7237c902bc2a3898efe65c93bc884e4211a49
samzyconcepts <samson.oderinwale@kibo.school>
2024-04-12 01:36:46 +0100

    Update: task can be mark completed, and deleted from the task component, Task can also be viewed

--- src/component/Task.tsx
+++ src/component/Task.tsx
@@ -1,15 +1,41 @@
+import { useDispatch } from "react-redux";
+import { toggleCompleted, removeTask } from "../state/task/taskSlice";
+
+// UI component
 import Button from "./ui/Button";
+// SVG
 import Delete from "../assets/Delete.svg";
 
-const Task = () => {
+interface Task {
+    id: string;
+    name: string;
+    completed: boolean;
+}
+
+const Task = ({ id, name, completed }: Task) => {
+    const dispatch = useDispatch();
+
     return (
         <div className="flex justify-between items-center py-3">
-            <label htmlFor="" className="flex gap-4 items-center">
-                <input type="checkbox" name="" id="" />
-                Buy new keyboard from Ikeja
+            <label htmlFor={id} className="flex gap-4 items-center">
+                <input
+                    type="checkbox"
+                    name={name}
+                    id={id}
+                    checked={completed}
+                    onChange={() => {
+                        dispatch(toggleCompleted(id));
+                    }}
+                />
+                <span
+                    className={`${
+                        completed ? "line-through decoration-2 decoration-[#855DDE]" : ""
+                    }`}>
+                    {name}
+                </span>
             </label>
 
-            <Button variant="secondary">
+            <Button variant="secondary" onClick={() => dispatch(removeTask(id))}>
                 <img src={Delete} alt="Delete button" />
             </Button>
         </div>
--- src/component/TasksView.tsx
+++ src/component/TasksView.tsx
@@ -1,10 +1,19 @@
 import Task from "./Task";
 
+import type { RootState } from "../state/store";
+import { useSelector } from "react-redux";
+
 function TasksView() {
-    return (
-        <section className="px-6 py-3 rounded-lg bg-white mt-2">
-            <Task />
-        </section>
-    );
+    const tasks = useSelector((state: RootState) => state.task.tasks);
+
+    if (tasks.length) {
+        return (
+            <section className="px-6 py-3 rounded-lg bg-white mt-2">
+                {tasks.map(({ id, name, completed }) => (
+                    <Task key={id} id={id} name={name} completed={completed} />
+                ))}
+            </section>
+        );
+    }
 }
 export default TasksView;
